b3b0532aeca9d2b1b4d016eafaa54831f90c25a0
ondolist <ondolist@naver.com>
2024-05-13 20:57:50 +0900

    change api key

--- env/settings.py
+++ env/settings.py
@@ -1,5 +1,5 @@
 LLM_API_KEY = {
-    'openai': 'sk-proj-RNdzBgeQSBYAg50Ym3nsT3BlbkFJNkgWY3fI9OMUAXpTRX3s', 
+    'openai': 'sk-proj-3e7ss6KFpAQpZbaeloXdT3BlbkFJzEmoCGgZXQJIF81HbBxk', 
     'anthropic': 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', 
     'gemini': 'AIzaSyDgUMo0qz_W9IcTi23D9Xc4-RO_RaGkLj0'
 }
