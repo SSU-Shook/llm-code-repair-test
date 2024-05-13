63f1f0a3c070263bd36ec41d3ca4ddf047279307
ondolist <ondolist@naver.com>
2024-05-13 20:45:20 +0900

    API KEY 업로드

---
+++ env/settings.py
@@ -0,0 +1,5 @@
+LLM_API_KEY = {
+    'openai': 'sk-proj-RNdzBgeQSBYAg50Ym3nsT3BlbkFJNkgWY3fI9OMUAXpTRX3s', 
+    'anthropic': 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', 
+    'gemini': 'AIzaSyDgUMo0qz_W9IcTi23D9Xc4-RO_RaGkLj0'
+}
